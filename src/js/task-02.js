6134b6ceb28094ad41acc653f6f619433f846f23
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-02-19 20:55:07 +0200

    add 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,44 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// const ingRef = document.querySelector('ul');
+// console.log(ingRef);
+
+// // const one = ingRef.createElement('li');
+
+
+// const liRef = document.createElement('li');
+// liRef.classList.add('item');
+// // liRef.createElement('p');
+
+// console.log(liRef);
+// const paragRef = document.createElement('p');
+// paragRef.textContent = "Potatoes";
+// // console.log(paragRef);
+
+// const addP = liRef.append(paragRef);
+
+// conts addUl = ingRef.append(addP);
+
+// console.log(ingRef);
+
+// const liRef = document.createElement('li');
+// liRef.classList.add('item');
+// liRef.textContent = 'Potatoes';
+// // console.log(liRef);
+
+// const ulRef = document.querySelector('ul');
+// ulRef.append(liRef);
+
+const linksRef = ingredients.map(el => {
+const liRef = document.createElement('li');
+liRef.classList.add('item');
+liRef.textContent = el;
+liRef.style.listStyleType = "none";
+  
+const ulRef = document.querySelector('ul');
+ulRef.append(liRef);
+  
+  return liRef;
+})
