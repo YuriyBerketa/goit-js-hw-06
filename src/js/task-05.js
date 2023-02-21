906cffc8848de993e6aeca13709b16b8a9354b41
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-02-21 11:07:35 +0200

    add 5

---
+++ src/js/task-05.js
@@ -0,0 +1,15 @@
+
+const refs = {
+    input: document.querySelector('#name-input'),
+    output: document.querySelector('#name-output'),
+}
+refs.input.addEventListener('input', onInputChange);
+
+
+function onInputChange(event) {
+    
+    refs.output.textContent = event.currentTarget.value.trim();
+    if (refs.output.textContent.length === 0) {
+        refs.output.textContent = 'Anonymous';
+    }
+}
