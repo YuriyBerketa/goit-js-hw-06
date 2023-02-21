3dcdec1bb4420a345a0debe8810650942fae5770
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-02-21 21:52:13 +0200

    add 6.1

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -2,7 +2,6 @@ const inputRef = document.querySelector('#validation-input');
 console.log(inputRef);
 inputRef.classList = 'validation-input';
 console.log(inputRef);
-// console.log(inputRef.textContent.length);
 const maxLength = inputRef.dataset.length;
 console.log(maxLength);
 
@@ -11,9 +10,11 @@ inputRef.addEventListener('blur', onInputValid);
 
 
 function onInputValid(event) {
-    if (event.currentTarget.value.length > maxLength) {
-        inputRef.classList.add('invalid');   
-    } else if (event.currentTarget.value.length > 0){
-        inputRef.classList.add('valid');
-    } 
+    // if (event.currentTarget.value.length > maxLength) {
+    //     inputRef.classList.add('invalid');   
+    // } else if (event.currentTarget.value.length > 0){
+    //     inputRef.classList.add('valid');
+    // } 
+    inputRef.classList = maxLength == event.currentTarget.value.length ? "valid" : "invalid";
+    
 }
