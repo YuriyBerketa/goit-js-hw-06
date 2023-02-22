f66a82a83d06c86516e47c7bcf78ef365c173243
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-02-22 10:50:56 +0200

    add 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,12 @@
+const formRef = document.querySelector('.login-form');
+console.log(formRef);
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const { email, password} = event.currentTarget.elements;
+    if (!(email.value && password.value)) alert('Заповніть всі поля!');
+    else { console.log({ email: email.value, password: password.value }) };
+    formRef.reset();
+};
 
