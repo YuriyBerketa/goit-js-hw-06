c7e0829f3d4eb425f1cdede49b79f44212cb84f8
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-02-22 11:15:51 +0200

    add 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const currentColor = document.querySelector('.color');
+const colorButton = document.querySelector('.change-color');
+
+colorButton.addEventListener('click', () => {
+  currentColor.textContent = document.body.style.backgroundColor = getRandomHexColor();
+});
